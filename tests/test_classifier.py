15fa47aaaf64f2ca2a65c689208753be20b2861d
sraddhanjali <3305962+sraddhanjali@users.noreply.github.com>
2025-02-28 16:57:44 -0800

    modified concept of pipeline outside LR clasS

---
+++ tests/test_classifier.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('.')  # Add folder to Python path
+from sklearn.datasets import make_blobs
+from sklearn.pipeline import Pipeline
+from . import ml_model as ml
+import matplotlib.pyplot as plt
+
+
+
+from tests.logistic_regression_classification import run_kfold, create_phi_matrix, preprocess_data
+from sklearn.model_selection import KFold
+import numpy as np
+from utils import features as ft
+from utils import preprocessing as pp
+from sklearn import datasets
+
+
+import sys
+sys.path.append('.')  # Add folder to Python path
+
+digits = datasets.load_digits()
+X_digits = digits.data
+y_digits = digits.target
+
+
+lr = ml.LogisticRegressionFromScratch()
+lr.fit(X_digits, y_digits)
+plt.axvline(lr.best_estimator_.named_steps['gradient_descent'],
+            linestyle=':', label='n_components chosen')
+plt.legend(prop=dict(size=12))
+plt.show()
