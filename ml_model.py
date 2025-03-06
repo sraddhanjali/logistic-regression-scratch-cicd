3b7c4e844c088854d1fd9769582d0837084a9716
sraddhanjali <3305962+sraddhanjali@users.noreply.github.com>
2025-03-05 16:49:13 -0800

    Close #8 remove feature transformation stage from classifier implementation to clean implementation

--- ml_model.py
+++ ml_model.py
@@ -87,48 +87,57 @@ class GradientDescentOptimizer:
 
 class LogisticRegressionFromScratch(BaseEstimator, TransformerMixin):
 
-    def __init__(self) -> None:
+    def __init__(self, n_class) -> None:
         # maintain state of learnable parameters over training etc.
         self.l_rate = CONFIG["l_rate"]
         self.max_iteration = CONFIG["max_iteration"]
         self.reg_param_lambda = CONFIG["reg_param_lambda"]
         self.weights = []
+        self.n_class = n_class
         self.opt = GradientDescentOptimizer()
 
-    def fit(self, X: np.ndarray, y: np.ndarray, n_class: int) -> 'LogisticRegressionFromScratch':
-        phi_matrix = PhiMatrixTransformer().fit(X)
-        print(f"Phi Matrix size: {phi_matrix.shape}")
-        target_vec = compute_target_vector(y, n_class)
-        print(f"y before : {y.shape} -  target vector shape: {target_vec.shape}")
-        self.opt = self.opt.fit(phi_matrix, target_vec, n_class)
+    def fit(self, X: np.ndarray, y: np.ndarray) -> 'LogisticRegressionFromScratch':
+        target_vec = compute_target_vector(y, self.n_class)
+        print(f"y before : {y.shape} -  target vector shape: {target_vec.shape} - n_class {self.n_class}") 
+        self.opt = self.opt.fit(X, target_vec, self.n_class)
         self.weights = self.opt.curr_weight
+        print(f'weights after fitting - {self.weights.shape}')
         self.is_fitted_ = True
         return self
 
+    def fit_transform(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        return self.fit(X, y).transform(X, y)
+
     def transform(self, X: np.ndarray, y: np.ndarray) -> 'LogisticRegressionFromScratch':
         check_is_fitted(self)
-        # print("fit got called", X.shape, y.shape)
         return self
         
     def summary(self) -> str:
         return "summary"
 
-    def predict(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
-        print("predict got called", X.shape)
-        X_transform = PhiMatrixTransformer().fit(X)
-        print("weights, ", len(self.weights))
-        y_log_t = (X_transform @ self.weights).T
+    def predict_(self, X: np.ndarray, y: np.ndarray=None) -> np.ndarray:
+        print(f"predict is being called on test_dataset")
+        y_target = compute_target_vector(y, self.n_class)
+        print(f"y before : {y.shape} -  target vector shape: {y_target.shape}")
+
+        y_log_t = (X @ self.weights).T
         print(y_log_t.shape)
         y_logistic = custom_softmax(y_log_t)
         print(y_logistic.shape)
         mapped_y_pred = mapping_prob_to_class(y_logistic)
-        print(f"Accuracy: {accuracy(y, mapped_y_pred) * 100}")
+        return mapped_y_pred
+
+    def predict(self, X: np.ndarray, y: np.ndarray=None) -> np.ndarray:
+        print(f"predict is being called on test_dataset")
+        y_log_t = (X @ self.weights)
+        print(f"Predictions {y_log_t.shape}")
+        y_logistic = custom_softmax(y_log_t)
+        print(f"custom softmaxed {y_logistic.shape}")
+        mapped_y_pred = mapping_prob_to_class(y_logistic)
+        # print(f"mapped prob to class {len(mapped_y_pred)} {mapped_y_pred}")
+        return mapped_y_pred
 
     def plot_convergence(self) -> None:
-        # cost_history = list(map(lambda x: x.to_list()[0], self.opt.cost_history))
-        # for i in range(len(self.cost_iterations)):
-            # self.cost_history[i] = self.cost_history[i].tolist()
-        # self.cost_history = [i[0] for i in self.cost_history]
         plt.plot(self.opt.cost_iterations, self.opt.cost_history)
         plt.title('Cost Function')
         plt.xlabel('Iterations')
