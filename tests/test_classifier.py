fdc15390df1f62316c54242f4a0592a16824f57f
sraddhanjali <3305962+sraddhanjali@users.noreply.github.com>
2025-03-05 16:50:55 -0800

    Test for ml_model close #8

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -17,7 +17,7 @@ from utils import preprocessing as pp
 from sklearn import datasets
 from sklearn.preprocessing import LabelBinarizer
 
-
+from sklearn.metrics import accuracy_score
 
 import sys
 sys.path.append('.')  # Add folder to Python path
@@ -32,11 +32,11 @@ X = scaler.fit_transform(X)
 
 
 # Train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train_, y_test_ = train_test_split(X, y, test_size=0.2, random_state=42)
 
 labelencoder_y = LabelBinarizer()
-y_train = labelencoder_y.fit_transform(y_train)
-y_test = labelencoder_y.fit_transform(y_test)
+y_train = labelencoder_y.fit_transform(y_train_)
+y_test = labelencoder_y.fit_transform(y_test_)
 # Initialize weight matrix
 n_features = X_train.shape[1]  # 64 pixels
 print(f"features {n_features}")
@@ -46,15 +46,18 @@ print(f"y_train {y_train.shape}")
 print(f"X_train {X_train.shape}")
 np.random.seed(42)
 weights = np.random.rand(n_features, n_classes)  # Shape: (64, 10)
-print(f"weights shape {weights.shape}")
+# print(f"weights shape {weights.shape}")
 # Hyperparameters
 learning_rate = 0.1
 max_iterations = 1000
 reg_lambda = 0.01
 
 
-lr = ml.LogisticRegressionFromScratch()
-lr.fit(X_train, y_train, n_classes)
-lr.predict(X_test, y_test)
+lr = ml.LogisticRegressionFromScratch(n_class=n_classes)
+lr.fit_transform(X_train, y_train)
+# print(f"original y_train {y_train_}")
+y_pred = lr.predict(X_train)
+print(f"Train accuracy score: {np.round(accuracy_score(y_train_, y_pred) * 100, 3)}")
 
-
+y_pred_ = lr.predict(X_test)
+print(f"Test accuracy score: {np.round(accuracy_score(y_test_, y_pred_) * 100, 3)}")
